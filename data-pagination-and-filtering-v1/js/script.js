713f46a15b3ad80d5f9364f97fe92c723263c2f1
brittonashford <brittonashford@users.noreply.github.com>
2021-11-29 21:24:20 -0600

    added pagination functionality, fixed some bugs

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -9,21 +9,6 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
-let studentList = document.querySelector('ul.student-list');
-let studentItem = document.createElement('li');
-studentItem.innerHTML = `
-            <li class="student-tem cf">
-                <div class="student-details">
-                    <img class="avatar" src=${data[0].picture.large} alt="Profile Picture">
-                    <h3>${data[0].name.title} ${data[0].name.first} ${data[0].name.last}</h3>
-                    <span class="email">${data[0].email}</span>
-                </div>
-                <div class="joined-details">
-                    <span class="date">${data[0].registered.date}</span>
-                </div>
-            </li>`;
-studentList.insertAdjacentHTML("beforeend", studentItem.innerHTML);
-
 
 function showPage(list, page) {
     const itemsPerPage = 9;
@@ -39,21 +24,55 @@ function showPage(list, page) {
             studentItem.innerHTML = `<li class="student-item cf">
                 <div class="student-details">
                     <img class="avatar" src=${data[i].picture.large} alt="Profile Picture">
-                    <h3>${data[i].name.title} ${data[i].name.first} ${data[i].name.last}</h3>
+                    <h3>${data[i].name.first} ${data[i].name.last}</h3>
                     <span class="email">${data[i].email}</span>
                 </div>
                 <div class="joined-details">
-                    <span class="date">${data[i].registered.date}</span>
+                    <span class="date">Joined: ${data[i].registered.date}</span>
                 </div>
             </li>`
 
-            console.log(studentItem.innerHTML);
             studentList.insertAdjacentHTML("beforeend", studentItem.innerHTML);
         }
     }
 }
 
+function addPagination(list) {
+    let linkList = document.querySelector('ul.link-list');
+    const numOfPages = Math.ceil(list.length / 9);
+
+    linkList.innerHTML = '';
+
+    for (i = 1; i <= numOfPages; i++) {
+        let button = document.createElement('li');
+        button.innerHTML =
+            `<li>
+                <button type="button">${i}</button>
+            </li>`;
+
+        linkList.insertAdjacentHTML('beforeend', button.innerHTML);
+    }
+
+    const firstPage = document.querySelector('button');
+    firstPage.className = "active";
+
+    linkList.addEventListener("click", (e) => {
+        console.log(e.target);
+        if (e.target.type === 'button') {
+            let activeBtn = document.querySelector('.active');
+            activeBtn.className = '';
+            e.target.className = 'active';
+            showPage(list, e.target.textContent);
+        }
+    })
+}
+
+
+
+
+
 showPage(data, 1);
+addPagination(data);
 
 
 /*
